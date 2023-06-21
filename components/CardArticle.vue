edb6f2bc06b0208e5c41c7e135111694b5b04b1f
WINDOWS-2OGN1V3\User <halditsa@gmail.com>
2023-06-21 12:03:40 +0700

    commit

---
+++ components/CardArticle.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="col-md-4">
+    <div
+      class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative"
+    >
+      <div class="article-img col-12 d-lg-block">
+        <img :src="article.image.small" alt="#" />
+      </div>
+      <div class="col-12 p-4 d-flex flex-column position-static">
+        <h4 class="mb-2">{{ article.title }}</h4>
+        <p class="card-text mb-auto">{{ article.contentSnippet }}</p>
+        <a :href="article.link" class="stretched-link" target="_blank"
+          >Continue reading</a
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    article: {
+      type: Object,
+      defaultValue: {
+        contentSnippet: "",
+        image: {
+          large: "",
+          small: "",
+        },
+        isoDate: "",
+        link: "",
+        title: "",
+      },
+    },
+  },
+};
+</script>
+
+<style scoped>
+.article-img img {
+width: 100%;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<script>
+import Header from "@/components/Header.vue";
+import Hero from "@/components/Hero.vue";
+import Article from "@/components/CardArticle.vue";
+export default {
+  components: {
+    Header,
+    Hero,
+    Article,
+  },
+  data() {
+    return {
+      articles: [],
+    };
+  },
+  async fetch() {
+    await this.$axios
+      .get("v1/cnn-news")
+      .then((res) => (this.articles = res.data.data));
+  },
+};
+</script>
+
+<template>
+  <div id="app">
+    <Header />
+    <main class="container">
+      <Hero />
+      <div class="row mb-2">
+        <Article
+          v-for="(article, index) in articles"
+          :key="index"
+          :article="article"
+        />
+      </div>
+    </main>
+  </div>
+</template>
